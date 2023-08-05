e5cec1e3f336a930a3168cf2db709f64f15d15cc
Abhishek Dangi <dangiabhi332@gmail.com>
2023-08-05 12:06:56 +0545

    employee admin design

--- employee/urls.py
+++ employee/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     path("registration/", registration, name="registration"),
     path("emp_login/", emp_login, name="emp_login"),
     path("emp_home/", emp_home, name="emp_home"),
+    path("emp_profile/", emp_profile, name="emp_profile"),
+    path("emp_logout/", emp_logout, name="emp_logout"),
+    
+    
     
 ]
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
 
-
 def index(request):
     return render(request, "index.html")
-
 
 def registration(request):
     error = ""
@@ -33,7 +31,6 @@ def registration(request):
 
     return render(request, "registration.html", locals())
 
-
 def emp_login(request):
     error = ""
     if request.method == "POST":
@@ -46,8 +43,58 @@ def emp_login(request):
         else:
             error = "yes"
 
-    return render(request, "emp_login.html", locals())
-
+    return render(request, "emp/emp_login.html", locals())
 
 def emp_home(request):
-    return render(request, "emp_home.html")
+    if not request.user.is_authenticated:
+        return redirect('emp_login')
+    return render(request, "emp/emp_home.html")
+
+def emp_profile(request):
+    if not request.user.is_authenticated:
+        return redirect('emp_login')
+    error = ""
+    user = request.user
+    employee = EmployeeDetail.objects.get(user=user)
+    if request.method == "POST":
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        empcode = request.POST["empcode"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        contact = request.POST["contact"]
+        department = request.POST["department"]
+        jdate = request.POST["jdate"]
+        designation = request.POST["designation"]
+        gender = request.POST["gender"]
+        
+        employee.user.first_name = firstname
+        employee.user.last_name = lastname
+        employee.empcode = empcode
+        employee.user.email = email
+        employee.contact = contact     
+        employee.designation = designation
+        employee.user.password = password
+        employee.department = department
+        
+        
+
+        try:
+            user = User.objects.create_user(
+                first_name=firstname,
+                last_name=lastname,
+                username=email,
+                password=password,
+                
+            )
+            EmployeeDetail.objects.create(user=user, empcode=empcode)
+            error = "no"
+        except:
+            error = "yes"
+
+    return render(request, "emp/emp_profile.html", locals())
+
+
+def emp_logout(request):
+    logout(request)
+    return redirect('index')
