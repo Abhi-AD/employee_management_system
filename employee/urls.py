c2c3237e8027fc3688ba314c283cc9edaf6082ee
Abhishek Dangi <dangiabhi332@gmail.com>
2023-08-11 15:08:11 +0545

    feat:employee admin planel

--- employee/urls.py
+++ employee/urls.py
@@ -26,15 +26,15 @@ urlpatterns = [
     path("emp_home/", emp_home, name="emp_home"),
     path("emp_profile/", emp_profile, name="emp_profile"),
     path("emp_logout/", emp_logout, name="emp_logout"),
-    path("admin_login/",admin_login, name="admin_login"),
-    path("emp_experiences/",emp_experiences, name="emp_experiences"),
-    path("emp_edit_experiences/",emp_edit_experiences, name="emp_edit_experiences"),
-    path("emp_education/",emp_education, name="emp_education"),
-    path("emp_edit_education/",emp_edit_education, name="emp_edit_education"),
-    path("change_password/",change_password, name="change_password"),
-    
-    
-    
+    path("admin_login/", admin_login, name="admin_login"),
+    path("emp_experiences/", emp_experiences, name="emp_experiences"),
+    path("emp_edit_experiences/", emp_edit_experiences, name="emp_edit_experiences"),
+    path("emp_education/", emp_education, name="emp_education"),
+    path("emp_edit_education/", emp_edit_education, name="emp_edit_education"),
+    path("change_password/", change_password, name="change_password"),
+    path("admin_home/", admin_home, name="admin_home"),
+    path("admin_change_password/",admin_change_password, name="admin_change_password"),
+    path("all_employee/",all_employee, name="all_employee"),
     
     
 ]
