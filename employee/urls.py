d08c7b9a4d1fe559581c7792d9eff00b5fc07160
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-09-12 22:32:14 +0545

    model post image show in template

--- employee/urls.py
+++ employee/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib.auth import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("employee_app.urls")),
     path("", include("web_app.urls")),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
